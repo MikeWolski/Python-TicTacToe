eb8c1567ec91cf539e296fadf3eb5379e975b2c0
Mike <wolski17@gmail.com>
2022-10-25 21:35:57 -0400

    Major Overhaul. Made a lot of breakthroughs

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,4 @@
-
 import pygame
-import time
 
 white = (255, 255, 255)
 red = (255, 0, 0)
@@ -9,25 +7,36 @@ blue = (0, 0, 255)
 green = (0, 255, 0)
 yellow = (255, 255, 102)
 
-dis_width = 1600
-dis_height = 1600
+dis_width = 1200
+dis_height = 1200
 
 clock = pygame.time.Clock()
 
 dis = pygame.display.set_mode((dis_width, dis_height), pygame.RESIZABLE)
 pygame.display.set_caption("Tic Tac Toe by Mike Wolski")
+dis.fill(white)
+pygame.draw.rect(dis, black, [(dis_width/3)-5, 0, 10, dis_height])
+pygame.draw.rect(dis, black, [(dis_width*2/3)-5, 0, 10, dis_height])
+pygame.draw.rect(dis, black, [0, (dis_height/3)-5, dis_width, 10])
+pygame.draw.rect(dis, black, [0, (dis_height*2/3)-5, dis_width, 10])
+pygame.display.update()
 
 game_over = False
 
-areaa = pygame.Rect(dis_width*0, dis_height*0, dis_width/3, dis_height/3)
-areab = pygame.Rect(dis_width/3, dis_width*0, dis_width/3, dis_height/3)
-areac = pygame.Rect(dis_width*2/3, dis_width*0, dis_width/3, dis_height/3)
-aread = pygame.Rect(dis_width*0, dis_height/3, dis_width/3, dis_height/3)
-areae = pygame.Rect(dis_width/3, dis_height/3, dis_width/3, dis_height/3)
-areaf = pygame.Rect(dis_width*2/3, dis_height/3, dis_width/3, dis_height/3)
-areag = pygame.Rect(dis_width*0, dis_height*2/3, dis_width/3, dis_height/3)
-areah = pygame.Rect(dis_width/3, dis_height*2/3, dis_width/3, dis_height/3)
-areai = pygame.Rect(dis_width*2/3, dis_height*2/3, dis_width/3, dis_height/3)
+class area():
+    def __init__(self, x, y, taken):
+        self.rect = pygame.Rect(dis_width*x, dis_height*y, dis_width*1/3, dis_height*1/3)
+        self.taken = taken
+
+areaa = area(0,0,0)
+areab = area(1/3,0,0)
+areac = area(2/3,0,0)
+aread = area(0,1/3,0)
+areae = area(1/3,1/3,0)
+areaf = area(2/3,1/3,0)
+areag = area(0,2/3,0)
+areah = area(1/3,2/3,0)
+areai = area(2/3,2/3,0)
 
 def circle(x, y):
     pygame.draw.circle(dis, blue, [dis_width*x, dis_height*y], dis_width/6.5, 10)
@@ -38,12 +47,7 @@ def square(x, y, a, b):
     pygame.draw.line(dis, red, [dis_width*x, dis_height*b], [dis_width*y, dis_height*a], 10)
     pygame.display.update()
 
-dis.fill(white)
-pygame.draw.rect(dis, black, [(dis_width/3)-5, 0, 10, dis_height])
-pygame.draw.rect(dis, black, [(dis_width*2/3)-5, 0, 10, dis_height])
-pygame.draw.rect(dis, black, [0, (dis_height/3)-5, dis_width, 10])
-pygame.draw.rect(dis, black, [0, (dis_height*2/3)-5, dis_width, 10])
-pygame.display.update()
+turn = 0
 
 while not game_over:
     for event in pygame.event.get():
@@ -51,44 +55,99 @@ while not game_over:
             game_over = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                if areaa.collidepoint(event.pos):
-                    square(0, 1/3, 0, 1/3)
-                elif areab.collidepoint(event.pos):
-                    square(1/3, 2/3, 0, 1/3)
-                elif areac.collidepoint(event.pos):
-                    square(2/3, 1, 0, 1/3)
-                elif aread.collidepoint(event.pos):
-                    square(0, 1/3, 1/3, 2/3)
-                elif areae.collidepoint(event.pos):
-                    square(1/3, 2/3, 1/3, 2/3)
-                elif areaf.collidepoint(event.pos):
-                    square(2/3, 1, 1/3, 2/3)
-                elif areag.collidepoint(event.pos):
-                    square(0, 1/3, 2/3, 1)
-                elif areah.collidepoint(event.pos):
-                    square(1/3, 2/3, 2/3, 1)
-                elif areai.collidepoint(event.pos):
-                    square(2/3, 1, 2/3, 1)
-            if event.button == 3:
-                if areaa.collidepoint(event.pos):
-                    circle(1/6, 1/6)
-                elif areab.collidepoint(event.pos):
-                    circle(1/2, 1/6)
-                elif areac.collidepoint(event.pos):
-                    circle(5/6, 1/6)
-                elif aread.collidepoint(event.pos):
-                    circle(1/6, 1/2)
-                elif areae.collidepoint(event.pos):
-                    circle(1/2, 1/2)
-                elif areaf.collidepoint(event.pos):
-                    circle(5/6, 1/2)
-                elif areag.collidepoint(event.pos):
-                    circle(1/6, 5/6)
-                elif areah.collidepoint(event.pos):
-                    circle(1/2, 5/6)
-                elif areai.collidepoint(event.pos):
-                    circle(5/6, 5/6)
-    clock.tick(1)
+                if turn%2 != 0:
+                    if areaa.rect.collidepoint(event.pos):
+                        if areaa.taken == 0:
+                            turn += 1
+                            square(0, 1/3, 0, 1/3)
+                            areaa.taken = 1
+                    elif areab.rect.collidepoint(event.pos):
+                        if areab.taken == 0:
+                            turn += 1
+                            square(1/3, 2/3, 0, 1/3)
+                            areab.taken = 1
+                    elif areac.rect.collidepoint(event.pos):
+                        if areac.taken == 0:
+                            turn += 1
+                            square(2/3, 1, 0, 1/3)
+                            areac.taken = 1
+                    elif aread.rect.collidepoint(event.pos):
+                        if aread.taken == 0:
+                            turn += 1
+                            square(0, 1/3, 1/3, 2/3)
+                            aread.taken = 1
+                    elif areae.rect.collidepoint(event.pos):
+                        if areae.taken == 0:
+                            turn += 1
+                            square(1/3, 2/3, 1/3, 2/3)
+                            areae.taken = 1
+                    elif areaf.rect.collidepoint(event.pos):
+                        if areaf.taken == 0:
+                            turn += 1
+                            square(2/3, 1, 1/3, 2/3)
+                            areaf.taken = 1
+                    elif areag.rect.collidepoint(event.pos):
+                        if areag.taken == 0:
+                            turn += 1
+                            square(0, 1/3, 2/3, 1)
+                            areag.taken = 1
+                    elif areah.rect.collidepoint(event.pos):
+                        if areah.taken == 0:
+                            turn += 1
+                            square(1/3, 2/3, 2/3, 1)
+                            areah.taken = 1
+                    elif areai.rect.collidepoint(event.pos):
+                        if areai.taken == 0:
+                            turn += 1
+                            square(2/3, 1, 2/3, 1)
+                            areai.taken = 1
+                elif turn%2 == 0:
+                    if areaa.rect.collidepoint(event.pos):
+                        if areaa.taken == 0:
+                            turn += 1
+                            circle(1/6, 1/6)
+                            areaa.taken = 1
+                    elif areab.rect.collidepoint(event.pos):
+                        if areab.taken == 0:
+                            turn += 1
+                            circle(1/2, 1/6)
+                            areab.taken = 1
+                    elif areac.rect.collidepoint(event.pos):
+                        if areac.taken == 0:
+                            turn += 1
+                            circle(5/6, 1/6)
+                            areac.taken = 1
+                    elif aread.rect.collidepoint(event.pos):
+                        if aread.taken == 0:
+                            turn += 1
+                            circle(1/6, 1/2)
+                            aread.taken = 1
+                    elif areae.rect.collidepoint(event.pos):
+                        if areae.taken == 0:
+                            turn += 1
+                            circle(1/2, 1/2)
+                            areae.taken = 1
+                    elif areaf.rect.collidepoint(event.pos):
+                        if areaf.taken == 0:
+                            turn += 1
+                            circle(5/6, 1/2)
+                            areaf.taken = 1
+                    elif areag.rect.collidepoint(event.pos):
+                        if areag.taken == 0:
+                            turn += 1
+                            circle(1/6, 5/6)
+                            areag.taken = 1
+                    elif areah.rect.collidepoint(event.pos):
+                        if areah.taken == 0:
+                            turn += 1
+                            circle(1/2, 5/6)
+                            areah.taken = 1
+                    elif areai.rect.collidepoint(event.pos):
+                        if areai.taken == 0:
+                            turn += 1
+                            circle(5/6, 5/6)
+                            areai.taken = 1
+    clock.tick(30)
 
 pygame.quit()
 quit()
